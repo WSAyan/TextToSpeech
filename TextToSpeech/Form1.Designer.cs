8d0e2b8c9688bef37d8c6bfdd4eb9b1e8d934091
wsayan <wsayan28@gmail.com>
2016-05-10 01:55:52 +0600

    texttospeech commited

---
+++ TextToSpeech/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TextToSpeech
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTextBox = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainTextBox
+            // 
+            this.mainTextBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.mainTextBox.Location = new System.Drawing.Point(0, 0);
+            this.mainTextBox.Multiline = true;
+            this.mainTextBox.Name = "mainTextBox";
+            this.mainTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.mainTextBox.Size = new System.Drawing.Size(246, 204);
+            this.mainTextBox.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(252, 66);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(108, 32);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pauseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(252, 104);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(108, 34);
+            this.pauseButton.TabIndex = 2;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = false;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(359, 204);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.mainTextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "TextToSpeech";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mainTextBox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+    }
+}
+
---
+++ TextToSpeech/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO; 
+
+namespace TextToSpeech
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer reader;
+        public Form1()
+        {
+            InitializeComponent();
+            pauseButton.Enabled = false;
+            reader = new SpeechSynthesizer();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            play();
+        }
+
+        //event handler 
+        void reader_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            startButton.Text = "Start";
+            pauseButton.Enabled = false;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                if (reader.State == SynthesizerState.Paused)
+                {
+                    reader.Resume();
+                    pauseButton.Enabled = true;
+                    pauseButton.Text = "Pause";
+                }
+                else if (reader.State == SynthesizerState.Speaking)
+                {
+                    reader.Pause();
+                    pauseButton.Enabled = true;
+                    pauseButton.Text = "Resume";
+
+                }
+            }
+        }
+
+        private void play()
+        {
+            reader.Dispose();
+            if (mainTextBox.Text != "")
+            {
+                startButton.Text = "Replay";
+                pauseButton.Text = "Pause";
+                reader = new SpeechSynthesizer();
+                reader.SpeakAsync(mainTextBox.Text.ToString().Trim());
+                pauseButton.Enabled = true;
+                reader.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>(reader_SpeakCompleted);
+            }
+            else
+            {
+                MessageBox.Show("Please enter some text in the textbox", "Message", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
